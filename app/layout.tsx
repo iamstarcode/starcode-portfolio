74e3463d4b6f2c905c6e4dfcd9c728c7318765e7
Bakare Abiola <iamstarcode@gmail.com>
2023-05-25 15:03:06 +0100

    add loading gif

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import '../styles/global.css';
-import '../public/antd.min.css';
-
-import CustomLink from '@/components/CustomLink';
 
 export const metadata = {
   title: 'Starcode',
@@ -10,9 +7,7 @@ export const metadata = {
 
 import { Quicksand } from 'next/font/google';
 import { Providers } from './providers';
-import { Suspense } from 'react';
 import NavBar from '@/layouts/partials/navbar';
-import Loading from './loading';
 
 const quicksand = Quicksand({
   subsets: ['latin'],
@@ -29,10 +24,10 @@ export default function RootLayout({
       <body className="m-0">
         <Providers>
           <div
-            className={`${quicksand.variable} font-quicksand bg-base-100 text-base-content`}
+            className={`${quicksand.variable} bg-base-100 font-quicksand text-base-content`}
           >
             <NavBar />
-            <div className="w-full max-w-6xl mx-auto px-4 py-12">
+            <div className="mx-auto w-full max-w-6xl px-4 py-12">
               {children}
             </div>
           </div>
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,13 @@
 import BlogSkeleton from '@/components/ui/BlogSkeleton';
+import Image from 'next/image';
+
 export default function Loading() {
   return (
     <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-base-100">
       <div className="flex flex-col items-center rounded-lg border bg-white px-5 py-2">
         <div className="mt-2 text-center text-xs font-light text-gray-500">
-          Please wait...
+          <Image height={64} width={64} src="/img/loading.gif" alt="loading" />
+          Loading...
           {/*  <BlogSkeleton /> */}
         </div>
       </div>
